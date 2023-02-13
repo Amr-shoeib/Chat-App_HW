3d1bb4331a1e59b545b87ed6a505c45ee4af4608
Amr-shoeib <amr79.a@gmail.com>
2023-02-13 00:38:58 -0500

    js separate file

---
+++ main.js
@@ -0,0 +1,37 @@
+    new Vue({
+        el: '#app',
+        data:{
+            connectedUsers:[],
+            messges:[],
+            messges:{
+                "type":"",
+                "action":"",
+                "user":"",
+                "text":"",
+                "timestamp":""
+
+            },
+            areTyping:[]
+        },
+
+        created: function(){
+
+        },
+
+         methods:{
+            send: function (){
+
+            },
+            userIsTtping: function (username){
+
+            },
+            userAreTyping: function (){
+
+            },
+            stoppedTyping: function (){
+
+            }
+        
+         }
+
+    })
