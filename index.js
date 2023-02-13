0240d5815a275ef749a51deb6f8dc26c35127100
Amr-shoeib <amr79.a@gmail.com>
2023-02-13 18:25:54 -0500

    server side connection

--- index.js
+++ index.js
@@ -11,19 +11,29 @@ console.log("Listening on *:" + port)
 app.use(express.static(__dirname));
 
 app.get('/', function(request, response){
+   
     response.sendFile(__dirname + '/index.html')
 });
 
 app.get('onlineusers', function (request, response){
-
+    console.log(io.socket.adapter.rooms);
+response.send(io.socket.adapter.rooms);
 });
 
 io.on('connection', function (socket){
 console.log('A user connected:' +socket.id);
 //Tell all users that someone connected
-io.emit('user joined', socket.id)
+io.emit('user joined', socket.id);
+
+// the user send chat messege to server
+socket.on('chat.message', function(message){
+    // emit this event to all users connected to it
+    io.emit('chat.message', message);
+})
 
 socket.on ('disconect', function(){
 console.log('user left:' + socket.id);
+//Tell all users that someone disconnected
+socket.broadcast.emit ('user left', socket.id);
 });
 });
--- js/main.js
+++ js/main.js
@@ -21,7 +21,17 @@
             socket.on('user joined', function (socketId){
                 this.connectedUsers.push(socketId);
             } .bind(this));
+
+          
+            // if user left then remove leaving user from connectUssers array
+            socket.on ('user left', function(socketId){
+                var index = this.connectedUsers.indexof(socketId);
+                if(index>=0){
+                    this.connectedUsers.splice(index,1);
+                }
+            });
         },
+
 
          methods:{
             send: function (){
